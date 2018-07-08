c4911634cbaf2734c8882140a51bb695ac137a99
Israel Mesa <mesa.israel50@gmail.com>
2018-07-07 20:21:41 -0400

    Model(Helper Class)

---
+++ src/com/webscraper/CoinDataUtil.java
@@ -0,0 +1,89 @@
+package com.webscraper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CoinDataUtil {
+
+	public static List<Coin> getCoins() {
+		
+		List<Coin> coins = new ArrayList<Coin>();
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/israelmesa/Desktop/ChromeDriver/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://finance.yahoo.com/cryptocurrencies?count=120");
+
+        System.out.println(driver.getTitle());
+
+        //Row
+
+        String xpathRow = "//*[@id=\"scr-res-table\"]/table/tbody/tr[*]";
+
+        List<WebElement> tableRows = driver.findElements(By.xpath(xpathRow));
+
+        //Symbol
+
+        String xpathSymbol = "//*[@id=\"scr-res-table\"]/table/tbody/tr[*]/td[1]";
+
+        List<WebElement> symbol = driver.findElements(By.xpath(xpathSymbol));
+
+        //Name
+
+        String xpathName = "//*[@id=\"scr-res-table\"]/table/tbody/tr[*]/td[2]";
+
+        List<WebElement> name = driver.findElements(By.xpath(xpathName));
+
+        //Price
+
+        String xpathPrice = "//*[@id=\"scr-res-table\"]/table/tbody/tr[*]/td[3]";
+
+        List<WebElement> price = driver.findElements(By.xpath(xpathPrice));
+
+        //Percent Change
+
+        String xpathPercentChange = "//*[@id=\"scr-res-table\"]/table/tbody/tr[*]/td[5]";
+
+        List<WebElement> percentChange = driver.findElements(By.xpath(xpathPercentChange));
+
+        //Market Cap
+
+        String xpathMarketCap = "//*[@id=\"scr-res-table\"]/table/tbody/tr[*]/td[6]";
+
+        List <WebElement> marketCap = driver.findElements(By.xpath(xpathMarketCap));
+
+        //Volume in Currency(24hr)
+
+        String xpathVolumeCurrency = "//*[@id=\"scr-res-table\"]/table/tbody/tr[*]/td[8]";
+
+        List <WebElement> volumeCurrency = driver.findElements(By.xpath(xpathVolumeCurrency));
+
+        //Circulating Supply
+
+        String xpathCirculatingSupply = "//*[@id=\"scr-res-table\"]/table/tbody/tr[*]/td[10]";
+
+        List<WebElement> circulatingSupply = driver.findElements(By.xpath(xpathCirculatingSupply));
+
+        int size = tableRows.size();
+
+        for (int i = 0; i < size; i++) {
+            coins.add(new Coin(symbol.get(i).getText(),
+            			name.get(i).getText(), 
+            			price.get(i).getText(), 
+            			percentChange.get(i).getText(), 
+            			marketCap.get(i).getText(), 
+            			volumeCurrency.get(i).getText(), 
+            			circulatingSupply.get(i).getText()));
+        }
+        
+        return coins;
+		
+	}
+	
+}
